c62ad8291024acd73f0da3fa4865ba56141b9476
iluo12 <820649664@qq.com>
2022-07-03 12:01:36 +0800

    C基础代码练习

---
+++ Reveiews/C/C_CodeFile/lesson1_初识C语言/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//std io - 标准输入输出
+int main()
+{
+	int num1;
+	int num2;
+	scanf("%d-%d", &num1, &num2);
+	printf("%d %d", num1, num2);
+	return 0;
+}
